43f7c517070d66e8805a1730768f34bd451463c1
Timothy Reed <treed25@hotmail.com>
2017-01-11 09:46:55 -0500

    support for foundation settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var path = require('path')
 
 module.exports = {
   entry: [
@@ -59,8 +60,13 @@ module.exports = {
         loader: 'file-loader',
         test:/\.(jpg|png)$/
 
-      }
+      },
     ]
   },
+  sassLoader: {
+      includePaths: [
+        path.resolve(__dirname, './node_modules/foundation-sites/scss')
+      ]
+    },
   devtool: 'cheap-module-eval-source-map'
 };
